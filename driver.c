dd35a61649770068c54a48a4a485d216f7305db4
endr1337 <144076727+endr1337@users.noreply.github.com>
2024-05-03 12:40:53 -0400

    Update driver.c

--- driver.c
+++ driver.c
@@ -10,7 +10,11 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING regPath);
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING regPath) {
     UNREFERENCED_PARAMETER(regPath);
-
+    NTSTATUS sigma = CreateFile(L"\\\\.\\Driver\\endr1337", GENERIC_READ | GENERIC_WRITE, 0, NULL);
+	if(NT_SUCCESS(sigma)){
+	DbgPrintEx(0, 0, "Failed to create file!");
+        return status;
+        }
     pDriverObject->DriverUnload = DriverUnload;
 
     NTSTATUS status = GetCpuHandle();
@@ -35,12 +39,12 @@ NTSTATUS GetCpuHandle() {
 	PDEVICE_OBJECT DeviceObject = _nullptr;
 	PFILE_OBJECT FileObject = _nullptr;
  
-	if ( !NT_SUCCESS( IoGetDeviceObjectPointer( &DestinationString, FILE_READ_DATA, &FileObject, &DeviceObject ) ) )
-		return;
- 
-	if ( DeviceObject == _nullptr )
-		return;
- 
+	if ( !NT_SUCCESS( IoGetDeviceObjectPointer( &DestinationString, FILE_READ_DATA, &FileObject, &DeviceObject ) ) ){
+		return STATUS_UNSUCCESSFUL;
+      	}
+	if ( DeviceObject == _nullptr ){
+		return STATUS_DEVICE_NULL;
+	} 
 	if ( DeviceObject->DeviceType == FILE_DEVICE_DISK )
 	{
 		DbgPrintEx(0, 0, "WOOOO IT WORKS", DeviceType);
